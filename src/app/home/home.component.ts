744a5f6d006629987e0caecce3edd493285d6c85
usersimran <simranjitsokhi4@gmail.com>
2024-02-19 17:17:52 +0530

    Api call code fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { ListService } from '../services/list.service';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,23 +13,11 @@ export class HomeComponent {
               private router: Router) { }
 
   ngOnInit() {
-    this.listservice.getList() .subscribe(
-      (data: any) => {  //Success callback
-        this.listdata = data.results;
-        console.log("list data:"+JSON.stringify(this.listdata));
-      },
-      (error: any) => {   //Error callback
-        console.error('error caught in component')
-        alert("Error while loading server data. Try Again");
-         }
-    )
-    
-    // this.listservice.getList().subscribe((data: any) => {
-    //   this.listdata = data.results;
-    //   console.log("list data:"+JSON.stringify(this.listdata));
-    // }
-    // );
+    this.listservice.getList().subscribe((data: any) => {
+      this.listdata = data.results;
+      console.log("list data:"+JSON.stringify(this.listdata));
+    }
+    );
   }
-
 
 }
